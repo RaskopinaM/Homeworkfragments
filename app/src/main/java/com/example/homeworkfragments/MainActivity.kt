7adde151b1f0678ffb2ec75f88e37bffaeb3c804
RaskopinaM <mariyaraskopina@yandex.ru>
2022-11-28 16:26:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homeworkfragments/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.homeworkfragments
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import com.example.homeworkfragments.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.fragment1Btn.setOnClickListener {
+            replaceFragment(Fragment1())
+        }
+
+        binding.fragment2Btn.setOnClickListener {
+            replaceFragment(Fragment2())
+
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+}
+
